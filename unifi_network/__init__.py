db28da7fcfe111b40eb624ec3e85190ebb1cf2a6
wittypluck <wittypluck@users.noreply.github.com>
2025-11-03 13:08:48 +0100

    Add async_remove_config_entry_device function to handle device removal

--- unifi_network/__init__.py
+++ unifi_network/__init__.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.device_registry import DeviceEntry
 
 from .const import DOMAIN, PLATFORMS
 from .core import UnifiNetworkCore
@@ -31,3 +32,28 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
     return unload_ok
+
+
+async def async_remove_config_entry_device(
+    hass: HomeAssistant, config_entry: ConfigEntry, device_entry: DeviceEntry
+) -> bool:
+    """Remove config entry from a device."""
+    core = hass.data[DOMAIN][config_entry.entry_id]
+
+    devices = (
+        core.device_coordinator.data
+        if core.device_coordinator and core.device_coordinator.data
+        else {}
+    )
+    clients = (
+        core.client_coordinator.data
+        if core.client_coordinator and core.client_coordinator.data
+        else {}
+    )
+
+    return not any(
+        identifier
+        for identifier in device_entry.identifiers
+        if identifier[0] == DOMAIN
+        and (identifier[1] in devices or identifier[1] in clients)
+    )
